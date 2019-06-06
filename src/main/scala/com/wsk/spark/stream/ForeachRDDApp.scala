0fa3b97d01de4dbfb50ad2df9311466a7db921af
sk <571325512@qq.com>
2019-06-06 10:31:10 +0800

    Spark Stream

--- src/main/scala/com/wsk/spark/stream/ForeachRDDApp.scala
+++ src/main/scala/com/wsk/spark/stream/ForeachRDDApp.scala
@@ -5,7 +5,13 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
-object DFSTreamWC {
+/**
+  * 1）foreachRDD是生产中用到的最多的output函数，通过它可将DStream转换为RDD以及DF\DS，然后进行操作
+  * 2）foreachRDD的方法是在driver中进行的，故写DB时它的获取连接代码必须写在第二层循环的foreachPartition中，不然会报序列化错误。
+  * 3）踩坑 ，在foreachPartition中数据的获取使用的是迭代器，不管是java还是Scala，迭代器只能使用一次，第二次就位空了
+  * 4）踩坑，操作DB时要使用连接池。
+  */
+object ForeachRDDApp {
   def main(args: Array[String]): Unit = {{
     val sc = new SparkConf()
       .setAppName("word count")
