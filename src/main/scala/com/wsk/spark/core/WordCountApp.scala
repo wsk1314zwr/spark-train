63ebe38fde9ad2dab5925b6211d3df2832f10987
sk <571325512@qq.com>
2019-07-27 13:25:55 +0800

    提交代码

--- src/main/scala/com/wsk/spark/core/WordCountApp.scala
+++ src/main/scala/com/wsk/spark/core/WordCountApp.scala
@@ -1,11 +1,12 @@
 package com.wsk.spark.core
 
+import org.apache.spark.internal.Logging
 import org.apache.spark.{SparkConf, SparkContext}
 
 /**
   * 可打包后用spark-submit部署的单词计数代码
   */
-object WordCountApp {
+object WordCountApp extends Logging{
 
   def main(args: Array[String]): Unit = {
 
@@ -16,10 +17,12 @@ object WordCountApp {
     val textRDD = sc.textFile("data/etlLog/input/hadoop-click-log.txt")
     val wc = textRDD.flatMap(_.split(","))
       .map((_,1))
-      .reduceByKey(_+_).collect()
+      .reduceByKey(_+_)
 
     //控制台打印输出
-//    wc.collect().foreach(println)
+    wc.collect().foreach(
+      x =>logError(s"输出结果：${x.toString()}")
+    )
 
     //输出到hdfs上,可以进行压缩
 //    wc.saveAsTextFile(args(1));
